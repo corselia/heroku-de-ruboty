c69c29104f2506f9641057c69c750e73fc09b798
corselia <takiya@toran.sakura.ne.jp>
2018-01-01 17:27:07 +0900

    [modify] modify the structure of original handlers
    the master file is my_ruboty.rb and each handler is in original_handlers/

--- my_ruboty.rb
+++ my_ruboty.rb
@@ -1,1 +1,16 @@
-require './ruboty_sushi'
+require './original_handlers/sushi'
+
+# 既存の gem を改造するのってこうやってオーバーライドするしかないのかなぁ
+module Ruboty
+  module Handlers
+    class GoogleImage < Base
+      on /画像( me)? (?<keyword>.+)/, name: "image", description: "Search image from Google"
+    
+      def image(message)
+        if url = search(message[:keyword])
+          message.reply("@#{message.from_name} #{url}")
+        end
+      end
+    end
+  end
+end
