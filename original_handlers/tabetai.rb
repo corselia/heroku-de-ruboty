a610059b345b734a4b7689f3849691c7b1705797
corselia <takiya@toran.sakura.ne.jp>
2018-01-01 19:08:23 +0900

    [add] add tabetai handler
    tabetai handler returns the image you wanna eat

---
+++ original_handlers/tabetai.rb
@@ -0,0 +1,21 @@
+module Ruboty
+  module Handlers
+    class Tabetai < Base
+      on(
+        /(<keyword>) 食べたい\z/i,
+        name: "tabetai",
+        description: "食べたいとつぶやいた食べ物の画像が投げつけられる"
+      )
+
+      def tabetai(message)
+        if url = search(message[:keyword])
+          message.reply("@#{message.from_name} ⊂( ･∀･) 彡 #{url}")
+        end
+      end
+
+      def search(query)
+        Ruboty::GoogleImage::Client.new(query: query).get
+      end
+    end
+  end
+end
